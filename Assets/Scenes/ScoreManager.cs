99596a3e53cf93d07a6d7a36e72ef571b7e745c1
Ebbe Wertz <ebbe.wertz8@gmail.com>
2025-10-14 13:59:44 +0200

    robin scoremanaget zit er ook bij nu

---
+++ Assets/Scenes/ScoreManager.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Nodig voor UI Text
+
+public class ScoreManager : MonoBehaviour
+{
+    public List<GameObject> destroyableObjects;
+    public GameObject scoreDisplay; // UI Text-object
+    public GameObject target;       // Wordt later gebruikt door jou
+
+    private int score = 0;
+    private Text scoreText;
+
+    void Start()
+    {
+        // Haal de Text-component op
+        scoreText = scoreDisplay.GetComponent<Text>();
+        if (scoreText != null)
+        {
+            scoreText.text = "Score: 0";
+        }
+    }
+
+    // Wordt aangeroepen door andere scripts als een object vernietigd wordt
+    public void AddScore(int amount)
+    {
+        score += amount;
+        if (scoreText != null)
+        {
+            scoreText.text = "Score: " + score;
+        }
+    }
+}
